9e8fda91447f82306350456309a714943c3ea254
Michaël Touati <14829604+michaeltouati@users.noreply.github.com>
2021-11-23 09:33:09 +0100

    Update maths_library.py

--- maths_library.py
+++ maths_library.py
@@ -1,7 +1,26 @@
-# Stopping Powers of Protons and Alpha particles in Ambiant Matter (SPAM)
-# written by Dr Michael J TOUATI - 09/27/2019 
-# mtouati@clpu.es
-
+#######################################################################
+##                                                                   ##
+##  Stopping Power of Protons and Alpha particles in Ambiant Matter  ##
+##                              (SPAM)                               ##
+##                                                                   ##
+## Copyright © 2020 Michaël J TOUATI                                 ##
+##                                                                   ##
+## This file is part of ESVM.                                        ##
+##                                                                   ##
+## SPAM is free software: you can redistribute it and/or modify      ##
+## it under the terms of the GNU General Public License as published ##
+## by the Free Software Foundation, either version 3 of the License, ##
+## or (at your option) any later version.                            ##
+##                                                                   ##
+## SPAM is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of    ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     ##
+## GNU General Public License for more details.                      ##
+##                                                                   ##
+## You should have received a copy of the GNU General Public License ##
+## along with ESVM. If not, see <https://www.gnu.org/licenses/>.     ##
+##                                                                   ##
+#######################################################################
 import math
 
 Euler_constant = 0.577215664901533
